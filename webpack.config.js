af367ebc888e4d16e64e5128a4fa8b8578d264f4
Nexuslolz <a.klyosov@inbox.ru>
2022-12-20 23:23:04 +0500

    fix: fix settings

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,6 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|svg|gif)$/i,
-                use: ['file-loader'],
                 type: 'asset/resource',
             },
             {
