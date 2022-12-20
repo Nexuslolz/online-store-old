c2946c139fb58add948795c822bd93e0e4313f78
Nexuslolz <a.klyosov@inbox.ru>
2022-12-20 23:27:25 +0500

    feat: update main page

--- src/pages/main/index.ts
+++ src/pages/main/index.ts
@@ -1,11 +1,25 @@
 import Page from '../../templates/page';
+import MainWrapper from './main-wrapper';
 
 class MainPage extends Page {
+    private wrapperLeft: MainWrapper;
+    private wrapperRight: MainWrapper;
+
     constructor(id: string) {
         super(id);
+        this.wrapperLeft = new MainWrapper(`left`);
+        this.wrapperRight = new MainWrapper(`right`);
     }
+
+    appendWrapper(): HTMLElement {
+        this.content.append(this.wrapperLeft.render());
+        this.content.append(this.wrapperRight.render());
+        return this.content;
+    }
+
     render(): HTMLElement {
-        console.log('main');
+        this.createWrapperContent();
+        this.appendWrapper();
         return this.container;
     }
 }
