b188d28ea02ceba2bab9738c624d95b68c9a7ea9
Nexuslolz <a.klyosov@inbox.ru>
2022-12-23 21:04:15 +0500

    refactor: fix error by commenting for future

---
+++ src/components/main/category-filter.ts
@@ -0,0 +1,21 @@
+// import dataBase from '../../data';
+// import Filter from '../../templates/filter';
+
+// class CategoryFilter extends Filter {
+//     constructor() {
+//         super();
+//         this.content = document.createElement('ul');
+//         this.content.classList.add('filter__content');
+//     }
+
+//     createElements(): HTMLDivElement {
+//         const categoryCheck = document.createElement('input') as HTMLInputElement;
+//         categoryCheck.classList.add('filter-category__item');
+//     }
+//     fillContent(): void {
+
+//     }
+//     appendElements(): HTMLDivElement {}
+
+//     render(): HTMLDivElement {}
+// }
