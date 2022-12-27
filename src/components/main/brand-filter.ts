161cce86b7367465d0f90ec6b56174c19f124bc9
Nexuslolz <a.klyosov@inbox.ru>
2022-12-27 13:48:26 +0500

    feat: add brand filter

---
+++ src/components/main/brand-filter.ts
@@ -0,0 +1,70 @@
+import dataBase from '../../data';
+import Filter from '../../templates/filter';
+
+class BrandFilter extends Filter {
+    constructor() {
+        super();
+        this.content = document.createElement('ul') as HTMLUListElement;
+        this.content.classList.add('filter__content');
+        this.content.classList.add('filter-list');
+    }
+
+    createElements(item: string): HTMLLIElement {
+        const filterListItem = document.createElement('li') as HTMLLIElement;
+        filterListItem.classList.add('filter-list__item');
+
+        const brandCheck = document.createElement('input') as HTMLInputElement;
+        brandCheck.classList.add('visually-hidden');
+        brandCheck.classList.add('filter-list__input');
+        brandCheck.id = `filter-${item}`;
+        brandCheck.setAttribute('type', 'checkbox');
+        brandCheck.setAttribute('name', 'filter');
+
+        const brandHeader = document.createElement('label') as HTMLLabelElement;
+        brandHeader.classList.add('filter-list__label');
+        brandHeader.setAttribute('for', `filter-${item}`);
+        brandHeader.textContent = `${item}`;
+
+        filterListItem.append(brandCheck);
+        filterListItem.append(brandHeader);
+
+        return filterListItem;
+    }
+
+    fillContent(): string[] {
+        this.header.textContent = `Бренд`;
+
+        const allbrandArr: string[] = [];
+
+        dataBase.products.forEach((elem) => {
+            allbrandArr.push(elem.brand);
+        });
+        const uniqueArr = [...new Set(allbrandArr)];
+
+        return uniqueArr;
+    }
+    appendElements(): HTMLDivElement {
+        this.wrapper.append(this.header);
+
+        const nameArr = this.fillContent();
+        if (this.content) {
+            for (let i = 0; i < nameArr.length; i++) {
+                const listItem = this.createElements(nameArr[i]);
+                const brandLength = 10;
+                if (this.content.querySelectorAll('.filter-list__item').length < brandLength) {
+                    this.content.append(listItem);
+                }
+            }
+            this.wrapper.append(this.content);
+        }
+        return this.wrapper;
+    }
+
+    render(): void {
+        const mainWrapper = document.querySelector('.left-content') as HTMLDivElement;
+        const newBlock = this.appendElements();
+        mainWrapper.append(newBlock);
+    }
+}
+
+export default BrandFilter;
