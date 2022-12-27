dd6b5a6056ab0f0fb5474db960a05d3323d993fb
Nexuslolz <a.klyosov@inbox.ru>
2022-12-27 13:48:06 +0500

    feat: add filter template

--- src/templates/filter.ts
+++ src/templates/filter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 abstract class Filter {
     protected wrapper: HTMLDivElement;
     protected header: HTMLElement;
@@ -7,24 +8,32 @@ abstract class Filter {
         this.wrapper = document.createElement('div');
         this.wrapper.classList.add('left-content__filter-wrapper');
         this.wrapper.classList.add('filter');
-        this.header = document.createElement('h3');
+        this.header = document.createElement('h2');
         this.header.classList.add('filter__header');
     }
 
-    createElements() {
-        return this.content;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    createElements(item?: string, direction?: string): HTMLLIElement | HTMLDivElement {
+        const filterListItem = document.createElement('li') as HTMLLIElement;
+        return filterListItem;
     }
 
-    fillContent() {
-        return;
+    createMoreElements?(value: string, min: number, max: number, direction: string): HTMLDivElement {
+        const valueWrapper = document.createElement('div');
+        return valueWrapper;
     }
 
-    appendElements() {
+    fillContent?(): string[] {
+        const allCategoryArr: string[] = [];
+        return allCategoryArr;
+    }
+
+    appendElements(item?: string, direction?: string, val?: string, min?: number, max?: number): HTMLDivElement {
         return this.wrapper;
     }
 
-    render() {
-        return this.appendElements();
+    render(): void {
+        return;
     }
 }
 
