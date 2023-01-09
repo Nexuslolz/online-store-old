03171bf2d373fd3505d81200bd76516145981520
Nexuslolz <a.klyosov@inbox.ru>
2023-01-10 04:14:57 +0500

    fix: fixed price maker

--- src/components/main/box-inherit/price-maker.ts
+++ src/components/main/box-inherit/price-maker.ts
@@ -7,13 +7,23 @@ function setFullPrice() {
     if (price.textContent !== null) {
         priceNum = Number(price.textContent.replace('$', ''));
     }
-    addBtn.forEach((btn, idx) => {
-        btn.addEventListener('click', () => {
-            const dataPrice = dataBase.products[idx].price;
-            if (btn.classList.contains('add-btn_active')) {
-                priceNum += dataPrice;
-            } else {
-                priceNum -= dataPrice;
+    addBtn.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+            let evt: Element | null | undefined;
+            if (event.target !== null && event.target instanceof HTMLElement) {
+                evt = event.target.closest('.card');
+            }
+
+            if (evt !== null && evt !== undefined) {
+                dataBase.products.forEach((item) => {
+                    if (evt !== null && evt !== undefined && item.id === Number(evt.id) + 1) {
+                        if (btn.classList.contains('add-btn_active')) {
+                            priceNum += dataBase.products[Number(evt.id)].price;
+                        } else {
+                            priceNum -= dataBase.products[Number(evt.id)].price;
+                        }
+                    }
+                });
             }
             price.textContent = `${priceNum}$`;
         });
